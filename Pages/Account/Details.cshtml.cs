4d4922d919932328ccee9963e259e6f4dacd0379
hainv405 <hainvhe160076@fpt.edu.vn>
2025-07-23 00:16:24 +0700

    gắn function vào front end
    homepage, detail

--- Pages/Account/Details.cshtml.cs
+++ Pages/Account/Details.cshtml.cs
@@ -1,12 +1,40 @@
+﻿using FoodioAPI.Database;
+using FoodioAPI.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace FoodieAPII.Pages.Account
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public DetailsModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public MenuItem SelectedItem { get; set; } // Món ăn hiện tại
+        public List<MenuItem> SuggestedItems { get; set; } // Gợi ý món ăn cùng loại
+
+        public async Task<IActionResult> OnGetAsync(Guid id)
+        {
+            // Truy vấn món ăn từ cơ sở dữ liệu theo ID
+            SelectedItem = await _context.MenuItems
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (SelectedItem == null)
+            {
+                return NotFound();
+            }
+
+            // Truy vấn các món ăn có cùng CategoryId
+            SuggestedItems = await _context.MenuItems
+                .Where(m => m.CategoryId == SelectedItem.CategoryId && m.Id != id) // Lọc theo CategoryId và loại trừ món hiện tại
+                .ToListAsync();
+
+            return Page();
         }
     }
 }
--- Pages/Account/Homepage.cshtml.cs
+++ Pages/Account/Homepage.cshtml.cs
@@ -1,3 +1,5 @@
+﻿using FoodioAPI.Database;
+using FoodioAPI.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,31 @@ namespace FoodieAPII.Pages.Account
 {
     public class HomepageModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomepageModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<MenuItem> HotDishes { get; set; }
+        public List<MenuItem> ColdDishes { get; set; }
+        public List<MenuItem> Drinks { get; set; }
+
         public void OnGet()
         {
+            // Lấy danh sách món ăn theo CategoryId, không theo tên
+            HotDishes = _context.MenuItems
+                .Where(m => m.CategoryId == new Guid("b74d406f-7390-41ce-952c-12ceb4374064"))  // CategoryId cho "Món Nóng"
+                .ToList();
+
+            ColdDishes = _context.MenuItems
+                .Where(m => m.CategoryId == new Guid("a4871081-e9da-4d5b-9596-aaea4981183a"))  // CategoryId cho "Món Lạnh"
+                .ToList();
+
+            Drinks = _context.MenuItems
+                .Where(m => m.CategoryId == new Guid("3eaf6dd5-5a69-4e56-a478-2b6f32ae456c"))  // CategoryId cho "Nước Uống"
+                .ToList();
         }
     }
 }
